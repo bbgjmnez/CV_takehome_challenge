787bf67e98bc10d85ca7a52fd216551901da50a8
Evan Ribnick <evan@revealtech.ai>
2024-05-10 20:01:42 +0000

    install Eigen, configure cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "Eigen/Dense"
+
 int main(int, char**){
     std::cout << "Hello, from cv_interview_challenge!\n";
+
+    Eigen::Vector3f vec(0,1,3);
+    std::cout << "vec: " << vec.transpose() << "\n";
 
 }
