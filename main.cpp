723ca543cd0ad428c4d2aac4cb00a607344b9f6a
Evan Ribnick <evan@revealtech.ai>
2024-05-10 22:00:15 +0000

    add nlohmann json

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 #include "Eigen/Dense"
+#include "nlohmann/json.hpp"
 
 int main(int, char**){
     std::cout << "Hello, from cv_interview_challenge!\n";
 
     Eigen::Vector3f vec(0,1,3);
     std::cout << "vec: " << vec.transpose() << "\n";
+
+    nlohmann::json json;
+    json["mood"] = "happy";
+    json["number"] = 6.3;
+    std::cout << "json:\n" << json.dump(4) << "\n";
 
 }
